211aef4cbba8ce1c1ee1f0dca3a3af3df073719e
JoashPrinceton <2410030361@klh.edu.in>
2026-04-10 11:06:01 +0530

    Add student year message to Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("I Love KLH");
+        System.out.println("I am a Student studing 2nd year");
     }
 }
