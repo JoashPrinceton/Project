613a558ad051122fbd53919110ac4829d05ad254
JoashPrinceton <2410030361@klh.edu.in>
2026-04-13 19:22:53 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("I Love KLH");
         System.out.println("I am a Student studing 2nd year");
+        System.out.println("I wrote exam Today.");
     }
 }
